16f3fd473fe816e07d7bb91bc4839d76bab2318d
Damarakiel <damarakiel@gmail.com>
2019-07-04 11:44:31 +0200

    [Feature] Disabled paralellism and removed copied tests

--- cypress/integration/citybreak_grouped copy 8.ts
+++
@@ -1,8 +0,0 @@
-describe('City Break - city to city', () => {
-
-    it('Visits grouped city break landing page', () => {
-        cy.viewport('iphone-6')
-        cy.visit(Cypress.config('baseUrl')+'/citybreak')
-    })
-    
-}) 
