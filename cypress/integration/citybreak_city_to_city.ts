1cfa4fffbfeb6f2fe82d5405936e5d82006bdffc
Damarakiel <damarakiel@gmail.com>
2019-07-04 09:37:00 +0200

    [Feature] Added new test

--- cypress/integration/citybreak_city_to_city.ts
+++ cypress/integration/citybreak_city_to_city.ts
@@ -1,16 +1,16 @@
-describe('City Break - city to city', () => {
+describe('City Break - city to city with offers', () => {
     
     beforeEach(() => {
         cy.server()
         cy.viewport('iphone-6')
     })
 
-    it('Visits KTW - BCN city break landing page', () => {
-        cy.visit(Cypress.config('baseUrl')+'/ci-ktw/ci-bcn')
+    it('Visits KTW - MIL city break landing page', () => {
+        cy.visit(Cypress.config('baseUrl')+'/ci-ktw/ci-mil')
     })
 
     it('Verifies heading', () => {
-        cy.get('div h1').contains('Barcelona City Break - Wylot z Katowic')
+        cy.get('div h1').contains('Mediolan City Break - Wylot z Katowic')
     })
 
     it('Verifies offers available', () => {
@@ -26,5 +26,40 @@ describe('City Break - city to city', () => {
 
         cy.get('li:nth-child(1) a[data-qa-offer]').click()
         cy.get('package-selected', {timeout: 30000}).should('exist')
+    })
+})
+
+describe('City Break - city to city without offers', () => {
+    
+    beforeEach(() => {
+        cy.server()
+        cy.viewport('iphone-6')
+    })
+
+    it('Visits KTW - BCN city break landing page', () => {
+        cy.visit(Cypress.config('baseUrl')+'/ci-waw/ci-bcn')
+    })
+
+    it('Verifies heading', () => {
+        cy.get('div h1').contains('Barcelona City Break - Wylot z Warszawy')
+    })
+
+    it('Verifies offers are not available', () => {
+        cy.get('a[data-qa-offer]').should('not.exist')
+        cy.get('div[data-qa-offers-teaser]').should('exist')
+    })
+
+    it('Verifies no offer description', () => {
+        cy.get('span[data-qa-offers-teaser-destination]').contains('Barcelona')
+        cy.get('span[data-qa-offers-teaser-title]').contains('zobacz ofertę najlepszych hoteli')
+    })
+
+    it('Verifies check offers button exists', () => {
+        cy.get('a[data-qa-offers-teaser-link]').should('exist')
+    })
+
+    it('Verifies search results are loading properly', () => {
+        cy.get('a[data-qa-offers-teaser-link]').click()
+        cy.get('.results .offers', {timeout: 30000}).should('exist')
     })
 })
